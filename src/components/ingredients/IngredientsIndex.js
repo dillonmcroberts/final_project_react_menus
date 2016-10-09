302d4c5b244d99b92d4ed934ae511bea72733500
dillonmcroberts <dillon.mcroberts@gmail.com>
2016-10-09 19:19:10 -0400

    ingredient index search working

--- src/components/ingredients/IngredientsIndex.js
+++ src/components/ingredients/IngredientsIndex.js
@@ -3,16 +3,49 @@ import {connect} from 'react-redux'
 import {Link} from 'react-router'
 
 
-function IngredientsIndex (props) {
-  return(
-    <div>
+class IngredientsIndex extends React.Component{
+  constructor(props) {
+    super(props)
 
-      <Link to='/ingredients/new'> Add a new ingredient</Link>
-      <ul>
-        {props.ingredients.map(ingredient => <Link to={`/ingredients/${ingredient.id}`}><li key={ingredient.id}>{ingredient.name}</li></Link>)}
-      </ul>
-    </div>
-  )
+    this.state = {term: ''}
+    this.searchBoxInput = this.searchBoxInput.bind(this)
+  }
+
+  makeIngredients() {
+  let ingredients = this.props.ingredients
+  let filteredIngredients = []
+
+  ingredients.forEach(ingredient => {
+    if (ingredient.name.toLowerCase().includes(this.state.term.toLowerCase()))
+    {
+      filteredIngredients.push(ingredient)
+    }
+  })
+
+
+    return filteredIngredients.map(ingredient => <Link to={`/ingredients/${ingredient.id}`}>
+      <h3 key={ingredient.id}>{ingredient.name}</h3>
+      <li> {ingredient.description}</li>
+      <li> Calories per tablespoon: {ingredient.caloriespertbsp}</li>
+      </Link>)
+  }
+
+
+
+
+  searchBoxInput(event){
+    this.setState({ term: event.target.value });
+  }
+
+    render(){
+    return(
+      <div>
+        <Link to='/ingredients/new'> Add a new ingredient</Link><br/>
+        <input value={this.state.term} placeholder="Search Ingredients" onChange={this.searchBoxInput}/>
+        {this.makeIngredients()}
+      </div>
+    )
+  }
 }
 
 function mapStateToProps(state){
