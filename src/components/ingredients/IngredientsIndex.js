a91ccd11569ac174f431528c9af7772c244401c7
dillonmcroberts <dillon.mcroberts@gmail.com>
2016-10-10 10:14:37 -0400

    update ingredient search

--- src/components/ingredients/IngredientsIndex.js
+++ src/components/ingredients/IngredientsIndex.js
@@ -25,12 +25,9 @@ class IngredientsIndex extends React.Component{
 
     return filteredIngredients.map(ingredient => <Link to={`/ingredients/${ingredient.id}`}>
       <h3 key={ingredient.id}>{ingredient.name}</h3>
-      <li> {ingredient.description}</li>
-      <li> Calories per tablespoon: {ingredient.caloriespertbsp}</li>
+
       </Link>)
   }
-
-
 
 
   searchBoxInput(event){
