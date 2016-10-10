e9da890c754e67f5df0d03bf16afe739d2b19f34
KhaledaNYC <khaleda.khan87@gmail.com>
2016-10-10 16:46:45 -0400

    minor formatting

--- src/components/SimpleSlider.js
+++ src/components/SimpleSlider.js
@@ -32,15 +32,15 @@ export default class SimpleSlider extends React.Component {
           <img className='featuredChef' src='https://media.licdn.com/media/AAEAAQAAAAAAAAhaAAAAJDkzMTQ4YWU3LTE0Y2YtNGMxNC05MTlkLTA5ZmI0Y2RlNmY3ZQ.jpg'></img>
             <h1>Chef Silvana </h1>
             <p>I make decorative chocolate cakes for Dominique Ansel</p>
-            <p><a className="btn btn-primary btn-lg">Learn more</a></p>
+            <p><a className="btn btn-primary btn-lg" href='/recipes/11'>Learn more</a></p>
          </div>
 
-        <div>
-          <img className='halloweenContest' src='https://files.slack.com/files-pri/T02MD9XTF-F2MFBFB9V/halloween-menu.jpg'/>
-</div>
         <div style={styles.slide}>
           <h2>Browse Fall Recipes and Menus</h2>
           <img className='fallRecipeImage' src='http://clv.h-cdn.co/assets/16/37/980x490/landscape-1473879046-picmonkey-collage-23.jpg'></img>
+      </div>
+      <div>
+        <img className='halloweenContest' src='https://files.slack.com/files-pri/T02MD9XTF-F2MFBFB9V/halloween-menu.jpg'/>
       </div>
       </Slider>
 
